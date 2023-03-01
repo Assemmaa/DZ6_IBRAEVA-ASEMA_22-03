3443fec31f2cabcc64e994ff8dea5cbdd625013a
Assemmaa <asemaibraeva88@gmail.com>
2023-03-01 13:53:01 +0600

    solve homework

---
+++ app/main.js
@@ -0,0 +1,48 @@
+const block = document.querySelector('.block')
+const btnPrev = document.querySelector('.btn-prev')
+const btnNext = document.querySelector('.btn-next')
+let count = 1
+
+btnNext.onclick = () => {
+    count++
+    fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+        .then(response => response.json())
+        .then(data => {
+            const div = document.createElement('div')
+            div.setAttribute('class', 'card')
+            div.innerHTML = `
+            <h2>${data.title}</h2>
+            <span>${data.id}</span>
+            <h3>${data.completed}</h3>
+        `
+            block.append(div)
+            btnPrev.onclick = () => {
+                if(count > 1) {
+                    count--
+                }
+                fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        const div = document.createElement('div')
+                        div.setAttribute('class', 'card')
+                        div.innerHTML = `
+            <h2>${data.title}</h2>
+            <span>${data.id}</span>
+            <h3>${data.completed}</h3>
+        `
+                        block.append(div)
+
+                    })
+            }
+        })
+}
+
+fetch('https://jsonplaceholder.typicode.com/todos', {
+    method: 'GET',
+    headers: {
+        'Content-type': 'application/json'
+    }
+}).then((response) => response.json()
+).then((data) => {
+    data.forEach(item => console.log(item))
+})
